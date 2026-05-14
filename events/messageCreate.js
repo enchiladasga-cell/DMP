5990615140088f6870164a844c8859e1640ed698
enchiladasga-cell <enchiladasga@gmail.com>
2026-05-14 12:17:12 -0600

    Change message reply to channel send

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -23,7 +23,7 @@ module.exports = {
     
     const respuesta = await askDeepseek(history);
     if (respuesta) {
-      await message.reply(respuesta);
+      await message.channel.send(respuesta);
     }
   }
 };
